15e5380e50e66044a8164064e1a083457a702f49
Joe <josephagrane@gmail.com>
2023-08-31 10:17:10 +0100

    removed docker items from this repo

--- docker/install_ros2.sh
+++
@@ -1,9 +0,0 @@
-apt update
-apt install software-properties-common curl -y
-add-apt-repository universe -y
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |  tee /etc/apt/sources.list.d/ros2.list > /dev/null
-apt update
-apt upgrade -y
-apt install ros-humble-ros-base ros-dev-tools -y
-
